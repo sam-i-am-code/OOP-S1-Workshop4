979c75a888599c01819efc7aaa9904eebe6f61bb
sam-i-am-code <downes95@hotmail.com>
2022-03-28 18:32:46 +1030

    q3-2

--- main-3-2.cpp
+++ main-3-2.cpp
@@ -6,7 +6,7 @@ extern int *reverseArray(int *numbers1, int length);
 int main(){
     int *p;
     p = readNumbers();
-    std::cout<<reverseArray(p,10);
+    reverseArray(p,10);
     delete [] p;
     return 0;
 }
