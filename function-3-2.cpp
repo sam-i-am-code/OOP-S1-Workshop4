0611b17f4a51ebae660f6e0ee56c943a2e440e97
sam-i-am-code <downes95@hotmail.com>
2022-03-28 18:38:16 +1030

    q3-2

--- function-3-2.cpp
+++ function-3-2.cpp
@@ -9,7 +9,14 @@ int *readNumbers(){
     }
     return array;
 }
-
+bool equalsArray(int *numbers1, int *numbers2, int length){
+    for(int i = 0; i < length; i++){
+        if(*(numbers1+i)!=*(numbers2+i)){
+            return false;
+        }
+    }
+    return true;
+}
 int *reverseArray(int *numbers1, int length){
     int *array = new int [10];
     for(int i = 0; i < length; i++){
--- main-3-2.cpp
+++ main-3-2.cpp
@@ -2,11 +2,15 @@
 
 extern int *readNumbers();
 extern int *reverseArray(int *numbers1, int length);
+extern bool equalsArray(int *numbers1, int *numbers2, int length);
 
 int main(){
     int *p;
+    int *j;
     p = readNumbers();
-    reverseArray(p,10);
+    j = reverseArray(p,10);
+    equalsArray(p,j,10);
     delete [] p;
+    delete [] j;
     return 0;
 }
