8e3069e1a22fdddbc84ed2b8b46e420af3776b41
sam-i-am-code <downes95@hotmail.com>
2022-03-28 18:06:53 +1030

    q2

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -14,63 +14,63 @@ void hexDigits(int *numbers,int length){
     for(int i = 0; i < 10; i++){
         switch (*(numbers+i)){
             case 1:
-            std::cout<<*(numbers+i)<<" "<< "1"<<std::endl;
+            std::cout<<i<<" "<<*(numbers+i)<<" "<< "1"<<std::endl;
             break;
             
             case 2:
-            std::cout<<*(numbers+i)<<" "<< "2"<<std::endl;
+            std::cout<<i<<" "<<*(numbers+i)<<" "<< "2"<<std::endl;
             break;
 
             case 3:
-            std::cout<<*(numbers+i)<<" "<< "3"<<std::endl;
+            std::cout<<i<<" "<<*(numbers+i)<<" "<< "3"<<std::endl;
             break;
             
             case 4:
-            std::cout<<*(numbers+i)<<" "<< "4"<<std::endl;
+            std::cout<<i<<" "<<*(numbers+i)<<" "<< "4"<<std::endl;
             break;
 
             case 5:
-            std::cout<<*(numbers+i)<<" "<< "5"<<std::endl;
+            std::cout<<i<<" "<<*(numbers+i)<<" "<< "5"<<std::endl;
             break;
             
             case 6:
-            std::cout<<*(numbers+i)<<" "<< "6"<<std::endl;
+            std::cout<<i<<" "<<*(numbers+i)<<" "<< "6"<<std::endl;
             break;
 
             case 7:
-            std::cout<<*(numbers+i)<<" "<< "7"<<std::endl;
+            std::cout<<i<<" "<<*(numbers+i)<<" "<< "7"<<std::endl;
             break;
             
             case 8:
-            std::cout<<*(numbers+i)<<" "<< "8"<<std::endl;
+            std::cout<<i<<" "<<*(numbers+i)<<" "<< "8"<<std::endl;
             break;
 
             case 9:
-            std::cout<<*(numbers+i)<<" "<< "9"<<std::endl;
+            std::cout<<i<<" "<<*(numbers+i)<<" "<< "9"<<std::endl;
             break;
             
             case 10:
-            std::cout<<*(numbers+i)<<" "<< "A"<<std::endl;
+            std::cout<<i<<" "<<*(numbers+i)<<" "<< "A"<<std::endl;
             break;
 
             case 11:
-            std::cout<<*(numbers+i)<<" "<< "B"<<std::endl;
+            std::cout<<i<<" "<<*(numbers+i)<<" "<< "B"<<std::endl;
             break;
             
             case 12:
-            std::cout<<*(numbers+i)<<" "<< "C"<<std::endl;
+            std::cout<<i<<" "<<*(numbers+i)<<" "<< "C"<<std::endl;
             break;
 
             case 13:
-            std::cout<<*(numbers+i)<<" "<< "D"<<std::endl;
+            std::cout<<i<<" "<<*(numbers+i)<<" "<< "D"<<std::endl;
             break;
             
             case 14:
-            std::cout<<*(numbers+i)<<" "<< "E"<<std::endl;
+            std::cout<<i<<" "<<*(numbers+i)<<" "<< "E"<<std::endl;
             break;
 
             case 15:
-            std::cout<<*(numbers+i)<<" "<< "F"<<std::endl;
+            std::cout<<i<<" "<<*(numbers+i)<<" "<< "F"<<std::endl;
             break;
         }
     }
